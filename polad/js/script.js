ebedf2809b1cc7d046980996bb1342674e2d1652
Cyril Zlachevsky <cyril.zlachevsky@gmail.com>
2016-11-13 15:41:21 +0200

    main menu changes

--- polad/js/script.js
+++ polad/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    $( '.dropdown' ).hover(
-        function(){
-            $(this).children('.sub-menu').slideDown(200);
-        },
-        function(){
-            $(this).children('.sub-menu').slideUp(200);
-        }
-    );
+    // $( '.dropdown' ).hover(
+    //     function(){
+    //         $(this).children('.sub-menu').slideDown(200);
+    //     },
+    //     function(){
+    //         $(this).children('.sub-menu').slideUp(200);
+    //     }
+    // );
 }); // end ready
